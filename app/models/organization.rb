4fb2c8e470987a9d1ad02b0b69c0ad1ac5928315
Alex <alex.stanga@me.com>
2014-06-26 20:38:14 -0600

    Add Org Users

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,11 @@
 class Organization < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
+  
 	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpeg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :organizations
   devise_for :users
   resources :events
 
